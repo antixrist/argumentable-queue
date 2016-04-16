13275ced0f1da079de41138145ad7ecac881b2f4
Kartashov Alexey <kartashov.al.and@yandex.ru>
2016-04-16 18:11:55 +0400

    .

--- test/index.js
+++ test/index.js
@@ -1,37 +1,91 @@
-var _         = require('lodash'),
-    slice     = require('sliced'),
-    hash      = require('object-hash'),
-    logTime   = require('../lib/log-time');
+var Queue   = require('../lib'),
+    logTime = require('../lib/log-time');
+
+var q = new Queue(function () {
+  console.log.apply(console, ['add new task with args:'].concat(arguments));
+});
+
+q.setOptions({
+  defaultPriority: 9000,
+  concurrency: function () {
+    console.log('concurrency this instanceof Queue', this instanceof Queue);
+    return '10';
+  },
+  throttle: function () {
+    console.log('throttle this instanceof Queue', this instanceof Queue);
+    return 200 + ' ';
+  },
+  debounce: ' '+ 300
+});
+
+q.on('add', function (task) {
+  console.log('on add', task);
+  console.log('============');
+});
 
 
-var calcTimeForNextTaskLastTime = null;
-var calcTimeForNextTaskValue    = null;
+var number = 0;
+var max = 1;
 
-var calcTimeForNextTask = function calcTimeForNextTask () {
-  return 1000;
-};
+logTime(max +' iterable', function (cb) {
+  var iterable = function iterable () {
+    setImmediate(function () {
+      number++;
 
-var getTimeForNextTask = function getTimeForNextTask () {
-  var time;
-  var now = Date.now();
+      q.add({
+        index: number
+      }, 'qwe');
 
-  // кэшируем вычисление оставшегося времени
-  if (!!calcTimeForNextTaskLastTime && now - calcTimeForNextTaskLastTime < calcTimeForNextTaskValue) {
-    console.log('cached');
-    time = calcTimeForNextTaskValue;
-  } else {
-    console.log('recalc');
-    time = calcTimeForNextTask();
-    calcTimeForNextTaskValue    = time;
-    calcTimeForNextTaskLastTime = now;
-  }
+      q.update({
+        //priority: q.options.defaultPriority - number
+      }, { index: number }, 'qwe');
 
-  return time;
-};
+      if (number < max) {
+        if (number % 100 == 0) {
+          console.log(number);
+        }
+        iterable();
+      } else {
+        //q.tasks.forEach(function (value, key) {
+        //  console.log('key:', key);
+        //  console.log('value:', value);
+        //  console.log('============');
+        //});
 
-setInterval(function () {
-  getTimeForNextTask();
-}, 200);
+        console.log('q.tasks', q.tasks);
+        console.log('q.size', q.size);
+        q.clear();
+        //console.log('q.tasks', q.tasks);
+
+        cb();
+      }
+    });
+  } ();
+});
+
+
+
+return;
+
+
+var queue = new Queue(function (url) {
+  return superagent.get(url).then(function (response) {
+    collectUrls(response).forEach(queue);
+  });
+}, {config: 123});
+
+queue('http://yandex.ru');
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -42,38 +96,113 @@ setInterval(function () {
 
 return;
 
-(function () {
-  var args = slice(arguments);
-  var arr = [];
 
-  logTime('hashing', function () {
-    var hashValue;
 
-    hashValue = {
-      index: 1,
-      args: args
-    };
-    //hashValue = Immutable.List();
+var assert = require('assert');
+var Promise = require('bluebird');
 
-    console.log('hashValue', hashValue);
-    console.log('hash', hash(hashValue));
 
-    return true;
+describe("Mocha Async Test", function(){
+  var q = new Queue(function () {
+
+  }, {
+
   });
 
-  return;
 
-  arr.push({
-    index: 1,
-    args: args
+  beforeEach(function(done){
+    setTimeout(function(){
+
+      // complete the async beforeEach
+      done();
+    }, 1500);
   });
 
-  var returnArgs = function returnArgs () {
-    return slice(arguments);
-  };
-
-  console.log(_.find(arr, {args: returnArgs(args)}));
-
-})({qwe: 123, method: function () { return null; }}, 'zxc', function (err, res) {
-  console.log('its callback');
+  it("flag should be true", function(){
+    assert(flag === true);
+  });
 });
+
+
+
+return;
+
+//var _         = require('lodash'),
+//    slice     = require('sliced'),
+//    hash      = require('object-hash'),
+//    logTime   = require('../lib/log-time');
+//
+//
+//var calcTimeForNextTaskLastTime = null;
+//var calcTimeForNextTaskValue    = null;
+//
+//var calcTimeForNextTask = function calcTimeForNextTask () {
+//  return 1000;
+//};
+//
+//var getTimeForNextTask = function getTimeForNextTask () {
+//  var time;
+//  var now = Date.now();
+//
+//  // кэшируем вычисление оставшегося времени
+//  if (!!calcTimeForNextTaskLastTime && now - calcTimeForNextTaskLastTime < calcTimeForNextTaskValue) {
+//    console.log('cached');
+//    time = calcTimeForNextTaskValue;
+//  } else {
+//    console.log('recalc');
+//    time = calcTimeForNextTask();
+//    calcTimeForNextTaskValue    = time;
+//    calcTimeForNextTaskLastTime = now;
+//  }
+//
+//  return time;
+//};
+//
+//setInterval(function () {
+//  getTimeForNextTask();
+//}, 200);
+//
+//
+//
+//
+//
+//
+//
+//
+//return;
+//
+//(function () {
+//  var args = slice(arguments);
+//  var arr = [];
+//
+//  logTime('hashing', function () {
+//    var hashValue;
+//
+//    hashValue = {
+//      index: 1,
+//      args: args
+//    };
+//    //hashValue = Immutable.List();
+//
+//    console.log('hashValue', hashValue);
+//    console.log('hash', hash(hashValue));
+//
+//    return true;
+//  });
+//
+//  return;
+//
+//  arr.push({
+//    index: 1,
+//    args: args
+//  });
+//
+//  var returnArgs = function returnArgs () {
+//    return slice(arguments);
+//  };
+//
+//  console.log(_.find(arr, {args: returnArgs(args)}));
+//
+//})({qwe: 123, method: function () { return null; }}, 'zxc', function (err, res) {
+//  console.log('its callback');
+//});
